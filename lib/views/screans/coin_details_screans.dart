99b9ab6e3f2fda64e5e42b8239557af4df01c373
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-03-09 21:00:29 +0330

    change detail screans

--- lib/views/screans/coin_details_screans.dart
+++ lib/views/screans/coin_details_screans.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: prefer_const_constructors_in_immutables
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:coinstats/models/bringcoins_model/data_model.dart';
 import 'package:coinstats/theme/constant.dart';
 import 'package:coinstats/util/view_models/app_provider.dart';
-import 'package:coinstats/views/widgets/widgets.dart';
+import 'package:coinstats/views/widgets/coin_detail_about_coin.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:provider/provider.dart';
 
 class CoinDetailsScreans extends StatelessWidget {
@@ -12,9 +14,9 @@ class CoinDetailsScreans extends StatelessWidget {
   final DataModel coin;
   @override
   Widget build(BuildContext context) {
+    var coinIconUrl =
+        "https://raw.githubusercontent.com/spothq/cryptocurrency-icons/master/128/color/";
     // ignore: unused_local_variable
-
-    var coinPrice = coin.quoteModel.usdModel;
 
     final appProvider = Provider.of<AppProvider>(context);
     final textTheme = Theme.of(context).textTheme;
@@ -24,7 +26,7 @@ class CoinDetailsScreans extends StatelessWidget {
         elevation: 0,
         foregroundColor: appProvider.brightness ? kwhite : kblack,
         title: Text(
-          coin.name + " / " + coin.symbol,
+          coin.name + " / " + "Rank #" + coin.cmcRank.toString(),
           style: textTheme.caption,
         ),
       ),
@@ -35,39 +37,70 @@ class CoinDetailsScreans extends StatelessWidget {
             const SizedBox(
               height: 40,
             ),
-            Text(
-              "\$" +
-                  coin.quoteModel.usdModel.price.toStringAsFixed(2).toString(),
-              style: textTheme.subtitle1,
+            SizedBox(
+              height: 100,
+              child: CachedNetworkImage(
+                imageUrl: ((coinIconUrl + coin.symbol + ".png").toLowerCase()),
+                placeholder: (context, url) =>
+                    const CircularProgressIndicator(),
+                errorWidget: (context, url, error) =>
+                    SvgPicture.asset('assets/icon/dollar.svg'),
+              ),
             ),
+            const SizedBox(
+              height: 25,
+            ),
+            Text(
+                "\$" +
+                    coin.quoteModel.usdModel.price
+                        .toStringAsFixed(2)
+                        .toString(),
+                style: TextStyle(
+                    color: appProvider.brightness ? kwhite : kblack,
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold)),
             Padding(
               padding: const EdgeInsets.only(top: 20),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Icon(
-                    coin.quoteModel.usdModel.percentChange_7d >= 0
+                    coin.quoteModel.usdModel.percentChange_1h >= 0
                         ? arrowupright
                         : arrowdownleft,
-                    color: coin.quoteModel.usdModel.percentChange_7d >= 0
+                    color: coin.quoteModel.usdModel.percentChange_1h >= 0
                         ? const Color.fromARGB(255, 17, 218, 50)
                         : Colors.red,
                   ),
                   Text(
-                      coin.quoteModel.usdModel.percentChange_7d
+                      coin.quoteModel.usdModel.percentChange_1h
                               .toStringAsFixed(2) +
-                          "%",
+                          "%" +
+                          "(1h)",
                       style: TextStyle(
-                          color: coin.quoteModel.usdModel.percentChange_7d >= 0
+                          color: coin.quoteModel.usdModel.percentChange_1h >= 0
                               ? Colors.green
                               : Colors.red,
-                          fontSize: 16)),
+                          fontSize: 18)),
                 ],
               ),
             ),
             const SizedBox(
-              height: 20,
+              height: 25,
             ),
+            CoinDetailAboutCoin(
+              deetial: coin.quoteModel.usdModel.marketCap.toString(),
+              name: 'Market cap',
+              datachcart: '',
+              icon: coin.quoteModel.usdModel.marketCap >= 0
+                  ? arrowupright
+                  : arrowdownleft,
+            ),
+            CoinDetailAboutCoin(
+                deetial: coin.quoteModel.usdModel.lastUpdated.toString(),
+                name: 'Last Update',
+                datachcart: '',
+                icon: Icons.timer_rounded),
           ],
         ),
       ),
---
+++ lib/views/widgets/coin_detail_about_coin.dart
@@ -0,0 +1,55 @@
+import 'package:coinstats/models/bringcoins_model/fetch_coins.dart';
+import 'package:coinstats/theme/constant.dart';
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class CoinDetailAboutCoin extends StatelessWidget {
+  // ignore: use_key_in_widget_constructors
+  const CoinDetailAboutCoin(
+      {required this.deetial,
+      required this.name,
+      required this.icon,
+      required this.datachcart});
+
+  final String name;
+  final String deetial;
+  final IconData icon;
+  final String datachcart;
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20, top: 15),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(name, style: const TextStyle(fontSize: 15, color: grey)),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Text(
+                deetial,
+                style: textTheme.subtitle2,
+              ),
+              Spacer(),
+              Icon(icon),
+              Text(datachcart)
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Divider(
+            indent: 2,
+            endIndent: 2,
+            color: kwhite,
+          )
+        ],
+      ),
+    );
+  }
+}
