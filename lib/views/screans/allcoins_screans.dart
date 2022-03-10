0d57b31f1efc2488577ea171f78dd158a3638504
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-03-10 19:38:52 +0330

    add NestedScrollView in All coins

--- lib/views/screans/allcoins_screans.dart
+++ lib/views/screans/allcoins_screans.dart
@@ -18,83 +18,89 @@ class AllCoinsScreans extends StatelessWidget {
     final size = MediaQuery.of(context).size;
 
     return Scaffold(
-      appBar: AppBar(
-          elevation: 0,
-          centerTitle: false,
-          actions: [
-            Padding(
-                padding: const EdgeInsets.only(right: Constans.padding),
-                child: IconButton(
-                  icon: const Icon(
-                    Icons.settings,
-                    size: 30,
-                    color: grey,
-                  ),
-                  onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (
-                          context,
-                        ) =>
-                            const DialogSettingScreans());
+      body: SafeArea(
+        child: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                automaticallyImplyLeading: false,
+                elevation: 0,
+                centerTitle: false,
+                title: Padding(
+                    padding: const EdgeInsets.only(left: 4),
+                    child: Text(
+                      'CoinStats',
+                      style: Theme.of(context).textTheme.subtitle1,
+                    )),
+                actions: [
+                  Padding(
+                      padding: const EdgeInsets.only(right: Constans.padding),
+                      child: IconButton(
+                        icon: const Icon(
+                          Icons.settings,
+                          size: 30,
+                          color: grey,
+                        ),
+                        onPressed: () {
+                          showDialog(
+                              context: context,
+                              builder: (
+                                context,
+                              ) =>
+                                  const DialogSettingScreans());
+                        },
+                      ))
+                ],
+              )
+            ];
+          },
+          body: ListView.builder(
+              shrinkWrap: true,
+              itemCount: coins.length,
+              itemBuilder: (context, index) {
+                var coin = coins[index];
+                var coinPrice = coin.quoteModel.usdModel;
+                var data = [
+                  ChartData(coinPrice.percentChange_90d, 2160),
+                  ChartData(coinPrice.percentChange_60d, 1440),
+                  ChartData(coinPrice.percentChange_30d, 720),
+                  ChartData(coinPrice.percentChange_24h, 24),
+                  ChartData(coinPrice.percentChange_1h, 1)
+                ];
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                CoinDetailsScreans(coin: coin)));
                   },
-                ))
-          ],
-          automaticallyImplyLeading: false,
-          title: Padding(
-            padding: const EdgeInsets.only(left: 4),
-            child: Text(
-              'CoinStats',
-              style: Theme.of(context).textTheme.subtitle1,
-            ),
-          )),
-      body: Scrollbar(
-        child: ListView.builder(
-            shrinkWrap: true,
-            itemCount: coins.length,
-            itemBuilder: (context, index) {
-              var coin = coins[index];
-              var coinPrice = coin.quoteModel.usdModel;
-              var data = [
-                ChartData(coinPrice.percentChange_90d, 2160),
-                ChartData(coinPrice.percentChange_60d, 1440),
-                ChartData(coinPrice.percentChange_30d, 720),
-                ChartData(coinPrice.percentChange_24h, 24),
-                ChartData(coinPrice.percentChange_1h, 1)
-              ];
-              return GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              CoinDetailsScreans(coin: coin)));
-                },
-                child: Padding(
-                  padding:
-                      const EdgeInsets.only(left: 20, right: 20, bottom: 16),
-                  child: Container(
-                      height: 70.0,
-                      width: size.width,
-                      padding: const EdgeInsets.only(top: 10),
-                      decoration: BoxDecoration(
-                          color: appProvider.brightness ? kblack : kwhite,
-                          borderRadius: BorderRadius.circular(16)),
-                      child: Row(
-                        children: [
-                          CoinLogoWidgets(
-                            coin: coin,
-                          ),
-                          CoinChartWidget(
-                            color: appProvider.brightness ? kwhite : kblack,
-                            data: data,
-                            coinPrice: coinPrice,
-                          )
-                        ],
-                      )),
-                ),
-              );
-            }),
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.only(left: 20, right: 20, bottom: 16),
+                    child: Container(
+                        height: 70.0,
+                        width: size.width,
+                        padding: const EdgeInsets.only(top: 10),
+                        decoration: BoxDecoration(
+                            color: appProvider.brightness ? kblack : kwhite,
+                            borderRadius: BorderRadius.circular(16)),
+                        child: Row(
+                          children: [
+                            CoinLogoWidgets(
+                              coin: coin,
+                            ),
+                            CoinChartWidget(
+                              color: appProvider.brightness ? kwhite : kblack,
+                              data: data,
+                              coinPrice: coinPrice,
+                            )
+                          ],
+                        )),
+                  ),
+                );
+              }),
+        ),
       ),
     );
   }
