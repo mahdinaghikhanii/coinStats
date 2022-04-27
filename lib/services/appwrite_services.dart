6b6bbbba7850e3bcacb0ac5c43e2961884d46732
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-04-27 22:17:34 +0430

    add manifast for appwrite in android

--- lib/services/appwrite_services.dart
+++ lib/services/appwrite_services.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 class AppwriteServices extends ChangeNotifier {
   var client = Client()
-      .setEndpoint("https://localhost/v1") // Your API Endpoint
+      .setEndpoint("http://localhost:4003/v1") // Your API Endpoint
       .setProject("6267e78a1181dfd0fe5b") // Your project ID
       .setSelfSigned(status: true);
 }
