abd6faee124dcf3a78a7c9abee6b95f2326e9638
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-05-16 15:10:54 +0430

    create in Settinfs UI

--- lib/views/screans/setting_screans.dart
+++ lib/views/screans/setting_screans.dart
@@ -1,6 +1,7 @@
-import 'package:coinstats/constant.dart';
 import 'package:flutter/material.dart';
 
+import '../../constant.dart';
+import '../../module/extension.dart';
 import '../widgets/builder_listtile_widgets.dart';
 
 class SettingScreans extends StatelessWidget {
@@ -8,23 +9,72 @@ class SettingScreans extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              color: kblue,
-              child: Text(),
+      backgroundColor: kblue,
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 40,
+          ),
+          SizedBox(
+            height: context.height * 0.14,
+            child: Text(
+              'Settings',
+              style: context.textTheme.subtitle2!
+                  .copyWith(fontWeight: FontWeight.bold, fontSize: 36),
+            ).center,
+          ),
+          Expanded(
+              child: Container(
+            decoration: BoxDecoration(
+              color: kwhite,
+              borderRadius: BorderRadius.circular(Constans.bigBorderRadios),
             ),
-            const BuilderListTileWidgets(
-              icon: Icons.person,
-              title: 'Account',
-            )
-          ],
-        ),
+            child: Padding(
+              padding: const EdgeInsets.all(Constans.padding),
+              child: Column(
+                children: const [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  BuilderListTileWidgets(
+                    icon: Icons.notifications,
+                    title: 'Help',
+                    showSwitchButton: true,
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  BuilderListTileWidgets(
+                    icon: Icons.privacy_tip_rounded,
+                    title: 'Privacy',
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  BuilderListTileWidgets(
+                    icon: Icons.share,
+                    title: 'Share with your frinds',
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  BuilderListTileWidgets(
+                    icon: Icons.person,
+                    title: 'About us',
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  BuilderListTileWidgets(
+                    icon: Icons.help,
+                    title: 'Help',
+                  ),
+                ],
+              ),
+            ),
+          )),
+        ],
       ),
     );
   }
--- lib/views/widgets/builder_listtile_widgets.dart
+++ lib/views/widgets/builder_listtile_widgets.dart
@@ -2,23 +2,68 @@ import 'package:coinstats/constant.dart';
 import 'package:flutter/material.dart';
 
 class BuilderListTileWidgets extends StatelessWidget {
-  const BuilderListTileWidgets(
-      {Key? key, required this.icon, required this.title})
-      : super(key: key);
+  const BuilderListTileWidgets({
+    Key? key,
+    required this.icon,
+    required this.title,
+    this.showSwitchButton = false,
+    this.onChanged,
+  }) : super(key: key);
 
   final IconData icon;
   final String title;
+  final bool showSwitchButton;
+  final VoidCallback? onChanged;
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Icon(
-        icon,
-        size: 30,
-        color: kblack,
+    return Container(
+      decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade400.withOpacity(0.1),
+              offset: const Offset(
+                5.0,
+                5.0,
+              ),
+              blurRadius: 10.0,
+              spreadRadius: 2.0,
+            ), //BoxShadow
+            const BoxShadow(
+              color: Colors.white,
+              offset: Offset(0.0, 0.0),
+              blurRadius: 0.0,
+              spreadRadius: 0.0,
+            )
+          ], //BoxShadow
+          border: Border.all(
+            width: 1.5,
+            color: Colors.grey.shade400.withOpacity(0.2),
+          ),
+          borderRadius: BorderRadius.circular(Constans.mediumBorderRadios)),
+      child: ListTile(
+        trailing: showSwitchButton
+            ? Switch(
+                inactiveTrackColor: kblue,
+                activeColor: kblue,
+                value: showSwitchButton,
+                onChanged: (val) {})
+            : const Icon(
+                Icons.keyboard_arrow_right,
+                size: 28,
+                color: Colors.grey,
+              ),
+        leading: Icon(
+          icon,
+          size: 30,
+          color: kblue,
+        ),
+        title: Text(title,
+            style: Theme.of(context).textTheme.subtitle1?.copyWith(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey.shade600)),
       ),
-      title: Text(title,
-          style: Theme.of(context).textTheme.subtitle1?.copyWith(fontSize: 20)),
     );
   }
 }
