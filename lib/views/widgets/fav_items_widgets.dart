a322c0d4e3de10d7b487ec14b392c58ad234d79c
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-05-21 22:25:32 +0430

    debugs

--- lib/views/widgets/fav_items_widgets.dart
+++ lib/views/widgets/fav_items_widgets.dart
@@ -1,3 +1,4 @@
+import 'package:coinstats/views/screans/coin_details_screans.dart';
 import 'package:coinstats/views/widgets/coin_logochart_widgets.dart';
 import 'package:coinstats/views/widgets/widgets.dart';
 import 'package:flutter/cupertino.dart';
@@ -18,53 +19,55 @@ class FavoriteIteamsWidgets extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<HiveForDataModel>(builder: (context, value, child) {
       context.watch<HiveForDataModel>().getFavorite();
-      return GestureDetector(
-        onTap: () {},
-        child: Expanded(
-            child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: value.listFavorite.length,
-                itemBuilder: (context, index) {
-                  DataModel fav = value.listFavorite[index];
-
-                  var coinPrice = fav.quoteModel.usdModel;
-                  var coinname = fav.name;
-                  var data = [
-                    ChartData(coinPrice.percentChange_90d, 2160),
-                    ChartData(coinPrice.percentChange_60d, 1440),
-                    ChartData(coinPrice.percentChange_30d, 720),
-                    ChartData(coinPrice.percentChange_24h, 24),
-                    ChartData(coinPrice.percentChange_1h, 1)
-                  ];
-                  return GestureDetector(
-                    onTap: () {},
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          left: 20, right: 20, bottom: 16),
-                      child: Container(
-                          height: 70.0,
-                          width: context.width,
-                          padding: const EdgeInsets.only(top: 10),
-                          decoration: BoxDecoration(
-                              color: appProvider.brightness ? kblack : kwhite,
-                              borderRadius: BorderRadius.circular(16)),
-                          child: Row(
-                            children: [
-                              CoinLogoWidgets(
-                                coin: fav,
-                              ),
-                              CoinChartWidget(
-                                color: kwhite,
-                                data: data,
-                                coinPrice: coinPrice,
-                              ),
-                              CoinDatilForPriceWidgets(coinPrice: coinPrice),
-                            ],
-                          )),
-                    ),
-                  );
-                })),
-      );
+      return SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: ListView.builder(
+              itemCount: value.listFavorite.length,
+              itemBuilder: (context, index) {
+                DataModel fav = value.listFavorite[index];
+                var coinPrice = fav.quoteModel.usdModel;
+                var data = [
+                  ChartData(coinPrice.percentChange_90d, 2160),
+                  ChartData(coinPrice.percentChange_60d, 1440),
+                  ChartData(coinPrice.percentChange_30d, 720),
+                  ChartData(coinPrice.percentChange_24h, 24),
+                  ChartData(coinPrice.percentChange_1h, 1)
+                ];
+                return GestureDetector(
+                  onTap: () {
+                    context.nextScreans(CoinDetailsScreans(coin: fav));
+                  },
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 20, right: 20, bottom: 16),
+                        child: Container(
+                            height: 70.0,
+                            width: context.width,
+                            padding: const EdgeInsets.only(top: 10),
+                            decoration: BoxDecoration(
+                                color: appProvider.brightness ? kblack : kwhite,
+                                borderRadius: BorderRadius.circular(16)),
+                            child: Row(
+                              children: [
+                                CoinLogoWidgets(
+                                  coin: fav,
+                                ),
+                                CoinChartWidget(
+                                  color: kwhite,
+                                  data: data,
+                                  coinPrice: coinPrice,
+                                ),
+                                CoinDatilForPriceWidgets(coinPrice: coinPrice),
+                              ],
+                            )),
+                      ),
+                    ],
+                  ),
+                );
+              }));
     });
   }
 }
