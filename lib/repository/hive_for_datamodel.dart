5fc068502852ebd2932256af00124684e736811e
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-05-21 16:18:55 +0430

    create hive_for_dataModel for saving coin detail and name and image

--- lib/repository/hive_for_datamodel.dart
+++ lib/repository/hive_for_datamodel.dart
@@ -1,1 +1,30 @@
-class HiveForDataModel {}
+import 'package:coinstats/models/bringcoins_model/fetch_coins.dart';
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+
+class HiveForDataModel with ChangeNotifier {
+  List _favoriteList = <DataModel>[];
+  List get listFavorite => _favoriteList;
+
+  int _lenghtFavorite = 0;
+  Future<void> countFavorite() async {
+    var box = await Hive.openBox<DataModel>('da');
+    _favoriteList = box.values.toList();
+    _lenghtFavorite = _favoriteList.length;
+    notifyListeners();
+  }
+
+  Future<void> addFavorite(DataModel dataModel) async {
+    var box = await Hive.openBox<DataModel>('da');
+    box.add(dataModel);
+    notifyListeners();
+  }
+
+  Future<void> removeFavorite() async {}
+
+  Future<void> getFavorite() async {
+    final box = await Hive.openBox<DataModel>('da');
+    _favoriteList = box.values.toList();
+    notifyListeners();
+  }
+}
